273f95c45c6a9f4f43179419dd8f206e30904086
BMelo-23 <131086678+BMelo-23@users.noreply.github.com>
2025-04-20 00:16:33 -0300

    solo atributos

--- Dominio/Avion.cs
+++ Dominio/Avion.cs
@@ -3,13 +3,9 @@ namespace Dominio
 	public class Avion
 	{
 		private string _fabricante;
-
 		private string _modelo;
-
 		private int _cantDeAsientos;
-
 		private double _alcance;
-
 		private decimal _costoOpXKm;
 
 	}
