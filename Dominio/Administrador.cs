53000aa330724179b9a4f1cf94636c831433fc00
BMelo-23 <131086678+BMelo-23@users.noreply.github.com>
2025-04-20 02:25:26 -0300

    Se finaliza la clase administrador

--- Dominio/Administrador.cs
+++ Dominio/Administrador.cs
@@ -1,4 +1,3 @@
-using Dominio;
 
 namespace Dominio
 {
@@ -6,7 +5,26 @@ namespace Dominio
 	{
 		private string _apodo;
 
-	}
+		public string Apodo
+		{
+			get { return _apodo; }
+		}
+		public Administrador(string email, string contrasenia, string apodo) : base(email, contrasenia)
+		{
+			_apodo = apodo;
+		}
 
+		public override void Validar()
+		{
+			base.Validar();
+			if (string.IsNullOrEmpty(_apodo)) throw new Exception("El podo no puede estar vacio");
+
+		}
+
+		public override string ToString()
+		{
+			return $"Apodo: {_apodo} Email: {_email}";
+		}
+	}
 }
 
