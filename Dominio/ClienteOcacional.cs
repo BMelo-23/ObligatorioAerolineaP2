4b695699f3b1b36761bedbecbb83e8afe86b437d
BMelo-23 <131086678+BMelo-23@users.noreply.github.com>
2025-04-20 01:46:08 -0300

    metodo geter, constructor y ToString

--- Dominio/ClienteOcacional.cs
+++ Dominio/ClienteOcacional.cs
@@ -1,10 +1,23 @@
-using Dominio;
 
 namespace Dominio
 {
 	public class ClienteOcacional : Cliente
 	{
-		private bool _regalosEnCanina;
+		private bool _elegibleParaRegalo;
+
+		public bool ElegibleParaRegalo
+		{
+			get { return _elegibleParaRegalo; }
+		}
+		public ClienteOcacional(string email, string contrasenia, string documento, string nombre, string nacionalidad, bool elegibleParaRegalo) : base(email, contrasenia, documento, nombre, nacionalidad)
+		{
+			_elegibleParaRegalo = elegibleParaRegalo;
+		}
+
+		public override string ToString()
+		{
+			return base.ToString() + $" - Elegible para regalo: {(_elegibleParaRegalo ? "Sí" : "No")}";
+		}
 
 	}
 
