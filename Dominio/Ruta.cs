fa622528faefcb5ec3f850b50eae7580d814c275
BMelo-23 <131086678+BMelo-23@users.noreply.github.com>
2025-04-20 02:23:51 -0300

    se crea metodo validar y se agregan atributos en constructor

--- Dominio/Ruta.cs
+++ Dominio/Ruta.cs
@@ -1,28 +1,40 @@
-using Dominio;
-
-namespace Dominio
+using Dominio.Interfaces;
+namespace Dominio                                               
 {
-	public class Ruta
+	public class Ruta : IValidable
 	{
+		private static int _contadorId = 1;
+		
 		private int _idRuta;
 		private Aeropuerto _aeroDeSalida;
 		private Aeropuerto _aeroDeLlegada;
 		private double _distancia;
-		private Aeropuerto[] aeropuerto;
+		//private Aeropuerto[] aeropuerto;
 
-		public string AeropuertoSalida
+		public int IdRuta
 		{
-			get { return _aeroDeSalida.CodigoIata; }
+			get { return _idRuta; }
 		}
-		public string AeropuertoDeLlegada
+		public Aeropuerto AeropuertoSalida
 		{
-			get { return _aeroDeLlegada.CodigoIata; }
+			get { return _aeroDeSalida; } //.CodigoIata; }
+		}
+		public Aeropuerto AeropuertoDeLlegada
+		{
+			get { return _aeroDeLlegada; } //.CodigoIata; }
+		}
+		public double Distancia
+		{
+			get { return _distancia; }
 		}
 		public Ruta(Aeropuerto aeroDeSalida, Aeropuerto aeroDeLlegada, double distancia)
 		{
-			_aeroDeSalida = aeroDeSalida;
+			_idRuta = _contadorId;
+			_contadorId++;
+			
+			_aeroDeSalida  = aeroDeSalida;
 			_aeroDeLlegada = aeroDeLlegada;
-			_distancia = distancia;
+			_distancia     = distancia;
 		}
 		public override bool Equals(object? obj)
 		{
@@ -31,10 +43,16 @@ namespace Dominio
 			if (obj.GetType() != GetType()) return false;
 			return Equals((Ruta)obj);
 		}
-
 		public override int GetHashCode()
 		{
 			return HashCode.Combine(_aeroDeSalida, _aeroDeLlegada, _distancia);
+		}
+
+		public void Validar()
+		{
+			if ( _aeroDeSalida  == null ) throw new Exception("El aeropuerto de salida no puede ser nulo");
+			if ( _aeroDeLlegada == null ) throw new Exception("El aeropuerto de llegada no puede ser nulo");
+			if ( _distancia <= 0 ) throw new Exception("la distancia debe ser mayor 0");
 		}
 	}
 
