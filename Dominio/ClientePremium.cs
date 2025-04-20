29300ab28dc493f20df66edc4bc4588c5e5f6138
BMelo-23 <131086678+BMelo-23@users.noreply.github.com>
2025-04-20 01:45:21 -0300

    Validaciones, constructor y geter

--- Dominio/ClientePremium.cs
+++ Dominio/ClientePremium.cs
@@ -1,4 +1,3 @@
-using Dominio;
 
 namespace Dominio
 {
@@ -6,6 +5,26 @@ namespace Dominio
 	{
 		private int _puntos;
 
+		public int Puntos
+		{
+			get {return _puntos;}
+		
+		}
+		public ClientePremium(string email, string contrasenia, string documento, string nombre, string nacionalidad, int puntos)
+			: base(email, contrasenia, documento, nombre, nacionalidad)
+		{
+			_puntos = puntos;
+		}
+
+		public override void Validar()
+		{
+			base.Validar();
+			if (_puntos < 0) throw new Exception("Los puntos deben deben ser mayor 0");
+		}
+		public override string ToString()
+		{
+			return base.ToString() + $"Puntos: {_puntos}";
+		}
 	}
 
 }
